2fae9e98e240ec7d169825f83fe48cf388865d5d
Anish Varshney <avanvarshney@gmail.com>
2021-01-01 17:11:03 +0530

    +login +signup route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
-
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <div className='app__body'>
+        <BrowserRouter>
+          {/* navbar */}
+          <Navbar />
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/dashboard' />
+            <Route exact path='/products' />
+            <Route exact path='/:pid' />
+            <Route exact path='/sell' />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 }
 
 export default App;
