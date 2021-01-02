e868812bfff0329da1139e2366917b8447283a29
Anish Varshney <avanvarshney@gmail.com>
2021-01-02 19:55:21 +0530

    +alerts

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -20,3 +20,7 @@ export const ITEM_FAIL = 'ITEM_FAIL';
 export const CLEAR_ITEM = 'CLEAR_ITEM';
 export const ITEM_BOUGHT = 'ITEM_BOUGHT';
 export const ITEM_BOUGHT_FAIL = 'ITEM_BOUGHT_FAIL';
+
+// alerts
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
