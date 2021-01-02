b0b2ceecda640bbd1fb0479300e773132de1a12c
Anish Varshney <avanvarshney@gmail.com>
2021-01-02 13:39:14 +0530

    +load user

---
+++ client/src/actions/auth.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import {
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+} from './types';
+import { setAuthToken } from '../utils/setAuthToken';
+
+// load user
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get('/auth');
+    dispatch({ type: USER_LOADED, payload: res.data });
+  } catch (error) {
+    dispatch({ type: AUTH_ERROR });
+  }
+};
+
+// login user
+export const login = (formData) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const body = JSON.stringify(formData);
+    const res = await axios.post('/auth/login', body, config);
+    dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+    dispatch(loadUser());
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: LOGIN_FAIL });
+  }
+};
+
+// signup user
+export const signup = (formData) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const data = JSON.stringify(formData);
+    const res = await axios.post('/auth/signup', data, config);
+    dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+    dispatch(loadUser());
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: REGISTER_FAIL });
+  }
+};
+
+//logout
+export const logout = () => async (dispatch) => {
+  try {
+    dispatch({ type: LOGOUT_SUCCESS });
+  } catch (error) {
+    dispatch({ type: LOGOUT_FAIL });
+  }
+};
