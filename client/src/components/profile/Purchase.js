9142c922bda94821786a2776b4119d7bdee5589b
Anish Varshney <avanvarshney@gmail.com>
2021-01-02 14:07:27 +0530

    +purchases

---
+++ client/src/components/profile/Purchase.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Purchase({ purchase }) {
+  const p_history = purchase.map((item) => (
+    <tr key={item._id}>
+      <td>{item.title}</td>
+      <td>{item.cost_price}</td>
+    </tr>
+  ));
+  return (
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th>Purchase price</th>
+        </tr>
+      </thead>
+      <tbody>{p_history}</tbody>
+    </table>
+  );
+}
+
+export default Purchase;
