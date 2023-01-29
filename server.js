28f7c15446d6d2774f9df9832ae12165f8961901
Anish Varshney <avanvarshney@gmail.com>
2023-01-30 03:06:01 +0530

    restored folder structure

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.json({ extended: false }));
 app.use('/auth/', require('./routes/auth'));
 app.use('/user/', require('./routes/user'));
 
-if (process.env.NODE_ENV === 'production') {
-  // server static assets
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", 'frontend', 'build', 'index.html'));
-  });
-}
+// server static assets
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server connected'));
