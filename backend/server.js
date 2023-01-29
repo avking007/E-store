ff033f9257bf3e90b7e30a93970e81ce1c9f0a57
Anish Varshney <avanvarshney@gmail.com>
2023-01-30 00:13:11 +0530

    fixed path

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,10 @@ app.use('/user/', require('./routes/user'));
 
 if (process.env.NODE_ENV === 'production') {
   // server static assets
-  app.use(express.static('client/build'));
+  app.use(express.static('../frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, "..", 'frontend', 'build', 'index.html'));
   });
 }
 const PORT = process.env.PORT || 5000;
